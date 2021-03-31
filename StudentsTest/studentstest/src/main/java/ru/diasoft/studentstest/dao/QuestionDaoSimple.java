e83b81a4dd903e4c737335c70d1ac0908f320e68
esharkova <esharkova@diasoft.ru>
2021-03-31 08:07:33 +0300

    откат

--- StudentsTest/studentstest/src/main/java/ru/diasoft/studentstest/dao/QuestionDaoSimple.java
+++
@@ -1,9 +0,0 @@
-package ru.diasoft.studentstest.dao;
-import ru.diasoft.studentstest.domain.Question;
-
-public class QuestionDaoSimple implements QuestionDao {
-
-    public Question findByNumber(int Number, String Question){
-        return new Question(Number, Question);
-    }
-}
